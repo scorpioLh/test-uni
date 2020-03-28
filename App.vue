d26cb173897f5b3dcddd92256a325d988fcd54db
linhan <scorpio360@163.com>
2020-03-28 23:31:50 +0800

    init

---
+++ App.vue
@@ -0,0 +1,17 @@
+<script>
+	export default {
+		onLaunch: function(option) {
+			console.log('App Launch', option)
+		},
+		onShow: function(option) {
+			console.log('App Show', option)
+		},
+		onHide: function() {
+			console.log('App Hide')
+		}
+	}
+</script>
+
+<style>
+	/*每个页面公共css */
+</style>
