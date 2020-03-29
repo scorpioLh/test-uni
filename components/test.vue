0c42b540f90db41a33b1c7275e7acf35ce878f7b
linhan <scorpio360@163.com>
2020-03-29 23:33:15 +0800

    feat:study

---
+++ components/test.vue
@@ -0,0 +1,36 @@
+<template>
+	<view>
+		{{ msg }}
+		<button type="primary" @click="test">Event</button>
+	</view>
+</template>
+
+<script>
+	export default {
+		props: {
+			msg: {
+				type: String,
+				default: ''
+			}
+		},
+		data() {
+			return {
+				
+			};
+		},
+		methods: {
+			test() {
+				// this.$emit('testShowName', {
+				// 	name: 'Name'
+				// })
+				uni.$emit('testEmit', {
+					name: 'lucy'
+				})
+			}
+		}
+	}
+</script>
+
+<style lang="scss">
+
+</style>
--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,52 +1,36 @@
 <template>
 	<view class="content">
-		<image class="logo" src="/static/logo.png"></image>
-		<view class="text-area">
-			<text class="title">{{title}}</text>
-		</view>
+		<!-- <test :msg="msg" @testShowName="testEvent" /> -->
+		<text>我是首页</text>
+		<view>{{ 'abc'.indexOf('ab') === -1 ? 'yes' : 'no' }}</view>
+		<image src="../../static/logo.png" mode=""></image>
 	</view>
 </template>
 
 <script>
+	import test from '../../components/test.vue'
+	
 	export default {
+		components: {
+			test
+		},
 		data() {
 			return {
-				title: 'Hello uni-app'
+				msg: 'I am test'
 			}
 		},
 		onLoad() {
-
+			// uni.$once('testEmit', (e) => {
+			// 	console.log(e)
+			// })
 		},
 		methods: {
-
+			// testEvent(e) {
+			// 	console.log(e)
+			// }
 		}
 	}
 </script>
 
 <style>
-	.content {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
-	}
-
-	.logo {
-		height: 200rpx;
-		width: 200rpx;
-		margin-top: 200rpx;
-		margin-left: auto;
-		margin-right: auto;
-		margin-bottom: 50rpx;
-	}
-
-	.text-area {
-		display: flex;
-		justify-content: center;
-	}
-
-	.title {
-		font-size: 36rpx;
-		color: #8f8f94;
-	}
 </style>
