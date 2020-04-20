002f4e5ed5e5e6cc664a493a4f7ffb686b31293e
linhan <scorpio360@163.com>
2020-04-21 00:08:55 +0800

    update

--- pages/news/news.vue
+++ pages/news/news.vue
@@ -1,6 +1,22 @@
 <template>
 	<view>
-		
+		<swiper :indicator-dots="true" :autoplay="true" :interval="3000" :duration="1000">
+			<swiper-item>
+				<view class="swiper-item">
+					<image scaleToFill src="https://img-cdn-qiniu.dcloud.net.cn/uniapp/images/shuijiao.jpg" mode=""></image>
+				</view>
+			</swiper-item>
+			<swiper-item>
+				<view class="swiper-item">
+					<image src="https://img-cdn-qiniu.dcloud.net.cn/uniapp/images/shuijiao.jpg" mode=""></image>
+				</view>
+			</swiper-item>
+			<swiper-item>
+				<view class="swiper-item">
+					<image src="https://img-cdn-qiniu.dcloud.net.cn/uniapp/images/shuijiao.jpg" mode=""></image>
+				</view>
+			</swiper-item>
+		</swiper>
 	</view>
 </template>
 
